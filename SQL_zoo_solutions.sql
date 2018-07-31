d9ad4cbf2b042638935b9fa8f1a68644b1330950
bubblebooy <38869011+bubblebooy@users.noreply.github.com>
2018-07-31 13:33:21 -0500

    Update SQL_zoo_solutions.sql
    Finished  SUM and COUNT

--- SQL_zoo_solutions.sql
+++ SQL_zoo_solutions.sql
@@ -200,6 +200,72 @@ SELECT name, continent FROM world x
                                     AND  population > 0)
 ) )
 
+
+### SUM and COUNT
+
+# 1
+ SELECT SUM(population)
+  FROM world
+# 2
+SELECT DISTINCT continent
+ FROM world
+# 3
+SELECT SUM(gdp)
+ FROM world
+  WHERE continent = 'Africa'
+# 4
+SELECT COUNT(name)
+ FROM world
+  WHERE  area > 1000000
+# 5
+SELECT SUM(population )
+ FROM world
+  WHERE  name IN ('Estonia', 'Latvia', 'Lithuania')
+# 6
+SELECT continent , COUNT(name)
+ FROM world
+  GROUP BY continent
+# 7
+SELECT continent , COUNT(name)
+ FROM world
+  WHERE population > 10000000
+  GROUP BY continent
+# 8
+SELECT continent
+ FROM world
+  GROUP BY continent
+   HAVING SUM(population) > 100000000
+   
+   
+### The JOIN operation
+
+# 1
+
+# 2
+
+# 3
+
+# 4
+
+# 5
+
+# 6
+
+# 7
+
+# 8
+
+# 9
+
+# 10
+
+# 11
+
+# 12
+
+# 13
+   
+
 ###
 
 # 1
