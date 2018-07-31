b5ca1cec505f2a8db5cbaeb7bea6a87c8f23bb8b
bubblebooy <38869011+bubblebooy@users.noreply.github.com>
2018-07-31 12:12:02 -0500

    Update SQL_zoo_solutions.sql
    Finnished SELECT from WORLD Tutorial

--- SQL_zoo_solutions.sql
+++ SQL_zoo_solutions.sql
@@ -9,8 +9,12 @@
   SELECT name, area FROM world
     WHERE area BETWEEN 200000 AND 250000 
     
+    
+### SELECT name
+  # how did this get skipped? 
+  
+  
 ### SELECT from WORLD Tutorial
-
 # 1
   SELECT name, continent, population FROM world
 # 2
@@ -58,3 +62,97 @@
       AND name LIKE '%o%'
       AND name LIKE '%u%'
   
+### SELECT from Nobel Tutorial
+# 1
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+ 
+# 1
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950
+# 2
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature'
+# 3
+SELECT yr ,subject
+  FROM nobel
+ WHERE winner =  'Albert Einstein'
+# 4
+SELECT winner
+  FROM nobel
+ WHERE yr >= 2000
+   AND subject = 'Peace'
+# 5
+SELECT *
+  FROM nobel
+ WHERE yr BETWEEN 1980 AND 1989 
+   AND subject = 'Literature'
+# 6
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter',
+                  'Barack Obama')
+# 7
+SELECT winner FROM nobel
+ WHERE winner LIKE 'John%'
+# 8
+SELECT * FROM nobel
+ WHERE yr = 1980 AND subject = 'Physics'
+  OR yr = 1984 AND subject = 'Chemistry'
+# 9
+SELECT * FROM nobel
+ WHERE yr = 1980
+  AND subject NOT IN ('Chemistry' , 'Medicine')
+# 10
+SELECT * FROM nobel
+ WHERE subject = 'Medicine' AND yr < 1910
+  OR subject = 'Literature' AND yr >= 2004
+# 11
+SELECT * FROM nobel
+ WHERE winner = 'PETER GRÜNBERG'
+# 12
+SELECT * FROM nobel
+ WHERE winner = 'EUGENE O''NEILL'
+# 13
+SELECT winner, yr, subject FROM nobel
+ WHERE winner LIKE 'sir %'
+  ORDER BY yr  DESC
+# 14
+SELECT winner, subject
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN ('Physics','Chemistry') , subject , winner
+###
+
+# 1
+
+# 2
+
+# 3
+
+# 4
+
+# 5
+
+# 6
+
+# 7
+
+# 8
+
+# 9
+
+# 10
+
+# 11
+
+# 12
+
+# 13
+
+# 14
