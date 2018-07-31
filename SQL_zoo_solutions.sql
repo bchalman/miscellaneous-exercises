4a3f0c2cbe6fc6aad5e434f9c5116a43b69ea2fd
bubblebooy <38869011+bubblebooy@users.noreply.github.com>
2018-07-31 17:32:34 -0500

    Update SQL_zoo_solutions.sql
    ###  NSS Tutorial

--- SQL_zoo_solutions.sql
+++ SQL_zoo_solutions.sql
@@ -457,7 +457,63 @@ SELECT teacher.name , CASE WHEN dept = 1 OR dept = 2 THEN 'Sci'
            ON (teacher.dept=dept.id)
 
 
-###
+
+###  NSS Tutorial
+
+# 1
+SELECT A_STRONGLY_AGREE
+  FROM nss
+ WHERE question='Q01'
+   AND institution='Edinburgh Napier University'
+   AND subject='(8) Computer Science'
+# 2
+SELECT institution , subject
+  FROM nss
+  WHERE question='Q15'
+    AND score >= 100
+# 3
+SELECT institution,score
+  FROM nss
+ WHERE question='Q15'
+   AND subject='(8) Computer Science'
+   AND score < 50
+# 4
+SELECT subject , SUM(response)
+  FROM nss
+  WHERE question='Q22'
+  GROUP BY subject
+    HAVING subject='(8) Computer Science'
+        OR subject='(H) Creative Arts and Design'
+# 5
+SELECT subject , SUM(A_STRONGLY_AGREE*response/100)
+  FROM nss
+  WHERE question='Q22'
+  GROUP BY subject
+    HAVING subject='(8) Computer Science'
+        OR subject='(H) Creative Arts and Design'
+# 6
+SELECT subject , ROUND(SUM(A_STRONGLY_AGREE*response) / SUM(response) )
+  FROM nss
+  WHERE question='Q22'
+  GROUP BY subject
+    HAVING subject='(8) Computer Science'
+        OR subject='(H) Creative Arts and Design'
+# 7
+SELECT institution , ROUND(SUM(score*response) / SUM(response))
+  FROM nss
+ WHERE question='Q22'
+   AND (institution LIKE '%Manchester%')
+ GROUP BY institution
+# 8
+SELECT institution , SUM(sample) , SUM(CASE WHEN subject = '(8) Computer Science' THEN sample END) AS comp
+  FROM nss
+ WHERE question='Q01'
+   AND (institution LIKE '%Manchester%')
+   # AND subject = '(8) Computer Science'
+   GROUP BY institution
+
+
+### Self join
 
 # 1
 
@@ -479,10 +535,3 @@ SELECT teacher.name , CASE WHEN dept = 1 OR dept = 2 THEN 'Sci'
 
 # 10
 
-# 11
-
-# 12
-
-# 13
-
-# 14
